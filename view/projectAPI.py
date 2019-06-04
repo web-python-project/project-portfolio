6fbfb014307614b50aedcb265d066917104d3a6e
seunghee63 <0603yang@naver.com>
2019-06-04 18:55:18 +0000

    connect homeProjectList to detailedProj

---
+++ view/projectAPI.py
@@ -0,0 +1,16 @@
+from flask import Blueprint,request, render_template, session, redirect, url_for
+from .db import mongo_connection, projectDAO
+
+db_connection = mongo_connection.ConnectDB().db
+proj0 = projectDAO.Project(db_connection)
+
+projAPI =  Blueprint('projAPI',__name__, template_folder='templates')
+
+@projAPI.route('/detailed', methods=['GET', 'POST'])
+def projDetailed():
+    if request.method == 'POST':
+        index = int(request.form["idx"])
+        result_pj = proj0.getOneProject(index)
+
+    return render_template('detailed.html',projInfo=result_pj)
+
