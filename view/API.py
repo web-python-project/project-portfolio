f658c10c1b1b6c8f179515a255e565689163a615
seunghee63 <0603yang@naver.com>
2019-06-11 16:17:39 +0000

    Change navbar contents according to login status and allow only admin to add project

--- view/API.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint,request, render_template, session, redirect, url_for
-
-commentAPI =  Blueprint('bookAPI',__name__, template_folder='templates')
-
-@commentAPI.route('/')
-def home():
-    return render_template('home.html')
